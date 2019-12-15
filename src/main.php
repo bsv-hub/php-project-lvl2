cf67649b3e77bb72e950b92ab0bfdbd43fcf623d
bsv-hub <bsv.phone@ya.ru>
2019-12-15 09:48:44 +0300

    Dirty solution to json diff

--- src/main.php
+++ src/main.php
@@ -11,7 +11,7 @@ Generate diff
 Usage:
   gendiff (-h|--help)
   gendiff (-v|--version)
-  gendiff [--format <fmt>] <firstFilePath> <secondFilePath>
+  gendiff [--format <fmt>] <pathToFile1> <pathToFile2>
 
 Options:
   -h --help                     Show this screen
@@ -21,5 +21,30 @@ DOC;
 
 function run()
 {
-    Docopt::handle(DOC, ['version' => VERSION]);
+    $response = Docopt::handle(DOC, ['version' => VERSION]);
+    $data1 = json_decode(file_get_contents($response->args['<pathToFile1>']), true);
+    $data2 = json_decode(file_get_contents($response->args['<pathToFile2>']), true);
+    $intersectElements = array_intersect_assoc($data1, $data2);
+    $addedElements = array_diff_assoc($data2, $data1);
+    $removedElements = array_diff_assoc($data1, $data2);
+
+    echo "{\n";
+    foreach ($intersectElements as $key => $value) {
+        echo "    {$key}: {$value}\n";
+    }
+    foreach ($removedElements as $key => $value) {
+        echo "  - {$key}: {$value}\n";
+        if (in_array($key, array_keys($addedElements))) {
+            echo "  + {$key}: {$addedElements[$key]}\n";
+        }
+    }
+    foreach ($addedElements as $key => $value) {
+        if (in_array($key, array_keys($removedElements))) {
+            continue;
+        }
+        echo "  + {$key}: {$addedElements[$key]}\n";
+    }
+    echo "}\n";
+
+    // print_r($response);
 }
